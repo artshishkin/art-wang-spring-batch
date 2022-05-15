bac8bac99c632d2ad01d2e5287e18f9da0bd36c9
Art <d.art.shishkin@gmail.com>
2022-05-15 12:31:19 +0300

    2.1 Hello Spring Batch - init project (#1 Section 1: Meet the Spring Batch)

---
+++ hello-world/src/test/java/net/shyshkin/study/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
