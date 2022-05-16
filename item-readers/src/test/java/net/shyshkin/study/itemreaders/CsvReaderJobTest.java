8683299b2ec1dfc6c7423243e9a7ca9db7bf27aa
Art <d.art.shishkin@gmail.com>
2022-05-16 17:16:34 +0300

    24.3 Testing reader Bean with StepScope (#3)

--- item-readers/src/test/java/net/shyshkin/study/itemreaders/CsvReaderJobTest.java
+++ item-readers/src/test/java/net/shyshkin/study/itemreaders/CsvReaderJobTest.java
@@ -1,15 +1,22 @@
 package net.shyshkin.study.itemreaders;
 
+import net.shyshkin.study.itemreaders.model.Product;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.core.*;
+import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.MetaDataInstanceFactory;
+import org.springframework.batch.test.StepScopeTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBatchTest
 @SpringBootTest
@@ -20,6 +27,9 @@ class CsvReaderJobTest {
 
     @Autowired
     private JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @Autowired
+    FlatFileItemReader<Product> itemReader;
 
     @AfterEach
     void tearDown() {
@@ -60,5 +70,32 @@ class CsvReaderJobTest {
                 .allSatisfy(execution -> assertThat(execution.getWriteCount()).isEqualTo(3));
         assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
 
+    }
+
+    @Test
+    void csvReaderStepScopeTest() throws Exception {
+        //given
+        StepExecution stepExecution = MetaDataInstanceFactory
+                .createStepExecution(defaultJobParameters());
+
+        //when
+        StepScopeTestUtils.doInStepScope(stepExecution, () -> {
+            Product productRead;
+            itemReader.open(stepExecution.getExecutionContext());
+            while ((productRead = itemReader.read()) != null) {
+
+                //then
+                Product product = productRead;
+                assertAll(
+                        () -> assertThat(product).hasNoNullFieldsOrProperties(),
+                        () -> assertThat(product.getProductID()).isGreaterThan(0),
+                        () -> assertThat(product.getPrice()).isGreaterThan(new BigDecimal("0.0")),
+                        () -> assertThat(product.getProductName()).isNotEmpty(),
+                        () -> assertThat(product.getProductDesc()).isNotEmpty()
+                );
+            }
+            itemReader.close();
+            return null;
+        });
     }
 }
