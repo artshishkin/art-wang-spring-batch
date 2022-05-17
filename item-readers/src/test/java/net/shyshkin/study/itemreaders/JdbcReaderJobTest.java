542ac86f312495ed1a0fb74256f9f07c39f72675
Art <d.art.shishkin@gmail.com>
2022-05-17 19:55:28 +0300

    32.3 Testing JDBC Cursor Item Reader (#3)

---
+++ item-readers/src/test/java/net/shyshkin/study/itemreaders/JdbcReaderJobTest.java
@@ -0,0 +1,87 @@
+package net.shyshkin.study.itemreaders;
+
+import net.shyshkin.study.itemreaders.config.BatchConfiguration;
+import net.shyshkin.study.itemreaders.config.DatabaseConfig;
+import net.shyshkin.study.itemreaders.config.JdbcReadBatchConfiguration;
+import net.shyshkin.study.itemreaders.model.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.*;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.test.MetaDataInstanceFactory;
+import org.springframework.batch.test.StepScopeTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@ContextConfiguration(classes = {BatchConfiguration.class, JdbcReadBatchConfiguration.class, DatabaseConfig.class})
+class JdbcReaderJobTest extends AbstractJobTest {
+
+    @Autowired
+    JdbcCursorItemReader<Product> itemReader;
+
+    private JobParameters defaultJobParameters() {
+        JobParametersBuilder paramsBuilder = new JobParametersBuilder();
+        return paramsBuilder.toJobParameters();
+    }
+
+    @Test
+    void jdbcReaderJobText() throws Exception {
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(defaultJobParameters());
+        JobInstance actualJobInstance = jobExecution.getJobInstance();
+        ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+        //then
+        assertThat(actualJobInstance.getJobName()).isEqualTo("jdbcReadJob");
+        assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
+
+    }
+
+    @Test
+    void jdbcReaderStepTest() {
+
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("readJdbc", defaultJobParameters());
+        var actualStepExecutions = jobExecution.getStepExecutions();
+        ExitStatus actualJobExitStatus = jobExecution.getExitStatus();
+
+        //then
+        assertThat(actualStepExecutions)
+                .hasSize(1)
+                .allSatisfy(execution -> assertThat(execution.getWriteCount()).isEqualTo(3));
+        assertThat(actualJobExitStatus.getExitCode()).isEqualTo("COMPLETED");
+
+    }
+
+    @Test
+    void jdbcReaderStepScopeTest() throws Exception {
+        //given
+        StepExecution stepExecution = MetaDataInstanceFactory
+                .createStepExecution(defaultJobParameters());
+
+        //when
+        StepScopeTestUtils.doInStepScope(stepExecution, () -> {
+            Product productRead;
+            itemReader.open(stepExecution.getExecutionContext());
+            while ((productRead = itemReader.read()) != null) {
+
+                //then
+                Product product = productRead;
+                assertAll(
+                        () -> assertThat(product).hasNoNullFieldsOrProperties(),
+                        () -> assertThat(product.getProductID()).isGreaterThan(0),
+                        () -> assertThat(product.getPrice()).isGreaterThan(new BigDecimal("0.0")),
+                        () -> assertThat(product.getProductName()).isNotEmpty(),
+                        () -> assertThat(product.getProductDesc()).isNotEmpty()
+                );
+            }
+            itemReader.close();
+            return null;
+        });
+    }
+}
