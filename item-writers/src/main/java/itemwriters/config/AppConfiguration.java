aa258b8b3a302ddc084b3b26444762ec4efb4d49
Art <d.art.shishkin@gmail.com>
2022-05-19 10:07:56 +0300

    40. Item Reader (#4)

--- item-writers/src/main/java/itemwriters/config/AppConfiguration.java
+++ item-writers/src/main/java/itemwriters/config/AppConfiguration.java
@@ -1,13 +1,31 @@
 package itemwriters.config;
 
+import itemwriters.model.Product;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class AppConfiguration {
 
-
+    @Bean
+    @StepScope
+    FlatFileItemReader<Product> flatFileItemReader(@Value("#{jobParameters['csvInputFile']}") FileSystemResource inputFile) {
+        return new FlatFileItemReaderBuilder<Product>()
+                .name("productCsvReader")
+                .resource(inputFile)
+                .linesToSkip(1)
+                .delimited()
+                .names("productID", "productName", "productDesc", "price", "unit")
+                .targetType(Product.class)
+                .build();
+    }
 }
