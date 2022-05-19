19229bc9aad95b944e14a743b79095417acd1d40
Art <d.art.shishkin@gmail.com>
2022-05-19 09:33:17 +0300

    39. Item Writer Project Init (#4 Section 4: Create Item Writer and Item Processor)

---
+++ item-writers/src/main/java/itemwriters/ItemWritersApplication.java
@@ -0,0 +1,17 @@
+package itemwriters;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+@RequiredArgsConstructor
+public class ItemWritersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemWritersApplication.class, args);
+    }
+
+}
