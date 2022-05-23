618228583187eaf2fd3febe057f6dc8d137ce978
Art <d.art.shishkin@gmail.com>
2022-05-23 19:06:16 +0300

    57.3 Retry Config (does not work) (#6)

---
+++ resilience/src/main/java/net/shyshkin/study/batch/resilience/config/RetryResilienceBatchConfiguration.java
@@ -0,0 +1,82 @@
+package net.shyshkin.study.batch.resilience.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.batch.resilience.exception.ProductServiceException;
+import net.shyshkin.study.batch.resilience.exception.Service500Exception;
+import net.shyshkin.study.batch.resilience.model.Product;
+import net.shyshkin.study.batch.resilience.service.ProductService;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@EnableBatchProcessing
+@Configuration
+@RequiredArgsConstructor
+public class RetryResilienceBatchConfiguration {
+
+    private final JobBuilderFactory jobs;
+    private final StepBuilderFactory steps;
+    private final ProductService productService;
+
+    @Bean
+    public Job retryJob() {
+        return jobs.get("retryJob")
+                .incrementer(new RunIdIncrementer())
+                .start(retryStep())
+                .build();
+    }
+
+    @Bean
+    Step retryStep() {
+        return steps.get("retryStep")
+                .<Product, Product>chunk(3)
+                .reader(serviceItemReader())
+                .writer(retryCsvItemWriter(null))
+                .faultTolerant()
+                .retryLimit(5)
+                .retry(Service500Exception.class)
+                .retry(ProductServiceException.class)
+                .build();
+    }
+
+    @Bean
+    ItemReader<? extends Product> serviceItemReader() {
+        return (ItemReader<Product>) productService::getProduct;
+    }
+
+    @Bean
+    @StepScope
+    FlatFileItemWriter<Product> retryCsvItemWriter(@Value("#{jobParameters['retryCsvOutputFile']}") FileSystemResource outputFile) {
+        return new FlatFileItemWriterBuilder<Product>()
+                .name("retryCsvProductWriter")
+                .resource(outputFile)
+                .delimited()
+                .delimiter("|")
+                .names("productID", "productName", "price", "unit", "productDesc")
+                .append(false)
+                .headerCallback(writer -> writer.write("productID|productName|price|unit|productDesc"))
+                .footerCallback(writer -> writer.write(
+                                String.format("The file was created %s\n",
+                                        LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+                        )
+                )
+                .build();
+    }
+
+}
