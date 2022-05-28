bce364ae4bd8436044db4d3d016f3f0791accf75
Art <d.art.shishkin@gmail.com>
2022-05-28 15:32:36 +0300

    73.3 Make BizTasklet4Failed configurable (#7)

--- high-performance/src/main/java/net/shyshkin/study/batch/performance/tasklet/BizTasklet4Failed.java
+++ high-performance/src/main/java/net/shyshkin/study/batch/performance/tasklet/BizTasklet4Failed.java
@@ -4,11 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@StepScope
 public class BizTasklet4Failed extends BizTasklet4 implements StepExecutionListener {
+
+    private final Boolean willComplete;
+
+    public BizTasklet4Failed(@Value("#{jobParameters['willComplete']}") Boolean willComplete) {
+        this.willComplete = willComplete;
+    }
 
     @Override
     public void beforeStep(StepExecution stepExecution) {
@@ -16,7 +25,12 @@ public class BizTasklet4Failed extends BizTasklet4 implements StepExecutionListe
 
     @Override
     public ExitStatus afterStep(StepExecution stepExecution) {
-        log.debug("After Step BizTasklet4Failed");
-        return ExitStatus.FAILED;
+
+        if (willComplete) {
+            return ExitStatus.COMPLETED;
+        } else {
+            log.debug("After Step BizTasklet4Failed");
+            return ExitStatus.FAILED;
+        }
     }
 }
